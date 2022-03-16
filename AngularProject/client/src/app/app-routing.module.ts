95dee674a71f371b44286d657f4df8c58b8754f6
Mayra Alejandra <91384030+maleja1122@users.noreply.github.com>
2022-03-16 11:04:05 -0500

    carritocompras

--- AngularProject/client/src/app/app-routing.module.ts
+++ AngularProject/client/src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { NavbarFiltroComponent } from './components/navbar-filtro/navbar-filtro.component';
 import { Pagina404Component } from './components/pagina404/pagina404.component';
 import { CarritoComprasComponent } from './components/carrito-compras/carrito-compras.component';
-
+import { FormularioPagoComponent } from './components/formulario-pago/formulario-pago.component';
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path: 'sistema', component: NavbarComponent},
   {path: 'sistemanavbar', component: NavbarFiltroComponent},
+  {path:'formulariopago', component: FormularioPagoComponent},
   {path: 'carritocompras',component:CarritoComprasComponent},
   {path:'404', component:Pagina404Component},
   {path: '**',redirectTo: '404',pathMatch: 'full'}
+
 ];
 
 @NgModule({
