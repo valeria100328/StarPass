dbc255158bf8736eafa1de8fa86936576ae62471
Mayra Alejandra <91384030+maleja1122@users.noreply.github.com>
2022-03-24 20:29:37 -0500

    Se hizo la descarga del pdf en la rama formulario pago, al presionar enviar, se descarga la boleta

--- AngularProject/client/src/app/components/formulario-pago/formulario-pago.component.ts
+++ AngularProject/client/src/app/components/formulario-pago/formulario-pago.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-formulario-pago',
@@ -11,4 +14,45 @@ export class FormularioPagoComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  async crearPdf(){
+    const definicion: any = {
+      content: [        
+        {
+          image: await this.getBase64ImageFromURL(
+            "../assets/img/Boleta.png"
+          ),
+          width:420,
+          height:600
+        }        
+      ]       
+    }
+    const pdf = pdfMake.createPdf(definicion);
+    // pdf.open()
+    pdf.download()
+  }
+
+  getBase64ImageFromURL(url) {
+    return new Promise((resolve, reject) => {
+      var img = new Image();
+      img.setAttribute("crossOrigin", "anonymous");
+    
+      img.onload = () => {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+    
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+    
+        var dataURL = canvas.toDataURL("image/png");
+    
+        resolve(dataURL);
+      };
+    
+      img.onerror = error => {
+        reject(error);
+      };
+    
+      img.src = url;
+    });}
 }
